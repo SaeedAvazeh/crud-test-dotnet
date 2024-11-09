4fd22cd2562d0e9a986c0decbc8538c34bdf3977
SaeedAvazeh <125880779+SaeedAvazeh@users.noreply.github.com>
2024-11-09 19:58:13 +0330

    Adding Blazor Web App to solutiuon

---
+++ Mc2.CrudTest.Presentation.Web/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Mc2.CrudTest.Presentation.Web.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
