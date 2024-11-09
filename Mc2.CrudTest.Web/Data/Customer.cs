03a5a38c8afe9ca075aa8fcd3d8ff5adef50aa89
SaeedAvazeh <125880779+SaeedAvazeh@users.noreply.github.com>
2024-11-09 21:18:33 +0330

    Adding Customer model data

---
+++ Mc2.CrudTest.Web/Data/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mc2.CrudTest.Web.Data
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateOnly DateOfBirth { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string BankAccountNumber { get; set; }
+    }
+}
