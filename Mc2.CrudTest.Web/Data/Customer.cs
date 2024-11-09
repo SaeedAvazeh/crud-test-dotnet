49a72c6d06ae9d0f1d64dbafc45e16e29aa43461
SaeedAvazeh <125880779+SaeedAvazeh@users.noreply.github.com>
2024-11-09 22:31:21 +0330

    Adding validation for phone number, email and bank account number with third party libraries

--- Mc2.CrudTest.Web/Data/Customer.cs
+++ Mc2.CrudTest.Web/Data/Customer.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using EmailValidation;
+using IbanNet;
+using PhoneNumbers;
+using System.ComponentModel.DataAnnotations;
 
 namespace Mc2.CrudTest.Web.Data
 {
@@ -16,12 +19,80 @@ namespace Mc2.CrudTest.Web.Data
         public DateOnly DateOfBirth { get; set; }
 
         [Required]
+        [ValidatePhoneNumber]
         public string PhoneNumber { get; set; }
 
         [Required]
+        [ValidateEmailAddress]
         public string Email { get; set; }
 
         [Required]
+        [ValidateBankAccountNumber]
         public string BankAccountNumber { get; set; }
+    }
+
+    public class ValidatePhoneNumber : ValidationAttribute
+    {
+        protected override System.ComponentModel.DataAnnotations.ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                var phoneNumberUtil = PhoneNumberUtil.GetInstance();
+                var phoneNumber = phoneNumberUtil.Parse(value.ToString(), null);
+                var regionCode = phoneNumberUtil.GetRegionCodeForNumber(phoneNumber);
+                if (!phoneNumberUtil.IsValidNumber(phoneNumber))
+                {
+                    return new System.ComponentModel.DataAnnotations.ValidationResult("the Phone Number is NOT Valid!");
+                }
+                return System.ComponentModel.DataAnnotations.ValidationResult.Success;
+            }
+            catch
+            {
+                return new System.ComponentModel.DataAnnotations.ValidationResult("the Phone Number is NOT Valid!");
+            }
+
+        }
+    }
+
+    public class ValidateBankAccountNumber : ValidationAttribute
+    {
+        protected override System.ComponentModel.DataAnnotations.ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                IIbanValidator validator = new IbanValidator();
+                IbanNet.ValidationResult validationResult = validator.Validate(value.ToString());
+                if (!validationResult.IsValid)
+                {
+                    return new System.ComponentModel.DataAnnotations.ValidationResult("the Bank Account Number is NOT Valid!");
+                }
+                return System.ComponentModel.DataAnnotations.ValidationResult.Success;
+            }
+            catch
+            {
+                return new System.ComponentModel.DataAnnotations.ValidationResult("the Bank Account Number is NOT Valid!");
+            }
+        }
+    }
+
+    public class ValidateEmailAddress : ValidationAttribute
+    {
+        protected override System.ComponentModel.DataAnnotations.ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            try
+            {
+                bool isValidEmailAddress = EmailValidator.Validate(value.ToString().Trim());
+
+                if (!isValidEmailAddress)
+                {
+                    return new System.ComponentModel.DataAnnotations.ValidationResult("the Email Address is NOT Valid!");
+                }
+                return System.ComponentModel.DataAnnotations.ValidationResult.Success;
+            }
+            catch
+            {
+                return System.ComponentModel.DataAnnotations.ValidationResult.Success;
+            }
+        }
     }
 }
